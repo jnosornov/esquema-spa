433bfc0b3d1d1f61857aca34e12ff37a4be56a22
Johnathan <jnosornov@gmail.com>
2019-03-01 21:22:38 -0500

    add collapsed content to tabs

---
+++ app/src/js/index.js
@@ -0,0 +1,10 @@
+// // selectors
+// var collapsedContentTabSelector = '.default-paragraph';
+// var contentTabSelector = '.content-description';
+
+// // elemetns
+// var collapsedContentTabElement = document.querySelector(collapsedContentTabSelector);
+// var contentTabElement = document.querySelector(contentTabSelector);
+
+// // contentTabElement.clientHeight = collapsedContentTabElement.clientHeight;
+// // console.log(collapsedContentTabElement.clientHeight);
